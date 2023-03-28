c07685b59b53c7661d78736add54170fda9417a1
Hiroki <sk38.tk924.nk826.sh1114@gmail.com>
2023-03-28 03:27:45 +0000

    php/laravel02

--- subject02.php
+++ subject02.php
@@ -1,13 +1,25 @@
 <?php
 
-$a = 3; $b = 7; echo $a + $b;
+/*1.$a　という変数に3を、$b　という変数に７を代入して、echoを使って$a+$bの結果を表示してみましょう*/
+$a = 3; $b = 7;
+echo $a + $b;
 
-$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]; echo $array_month[7];
+/*2.$array_month という配列に1月〜12月の文字列を代入し、echoで8月を表示してみましょう*/
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
 
-$hello = "Hello"; $name = "Hirok"; $World = "'sWorld!"; echo $hello . $name . $World;
+/*3.$hello　という変数に　”Hello, “を $name という変数にあなたの名前を、　
+$world という変数に　”‘s World!” を代入して、各変数を連結させて “Hello, あなたの名前’s World!” と表示してみましょう*/
+$hello = "Hello";
+$name = "Hirok";
+$World = "'sWorld!"; echo $hello . $name . $World;
 
-$tech_boost = 'tech'; $tech_boost .= 'boost'; echo $tech_boost;
+/*4.$tech_boostに “tech “という文字列が代入されています。複合演算を使って、echo $tech_boostと実行すると「tech boost」と表示されるように実装してみましょう*/
+$tech_boost = 'tech';
+$tech_boost .= 'boost';
+echo $tech_boost;
 
+/*5.【応用】次のプログラムにはバグがあります。どこにバグが有るか調べて修正してみましょう(バグは一つとは限りません。*/
 $array_2018_calender = [
   "January" => "1月",
   "February" => "2月",
